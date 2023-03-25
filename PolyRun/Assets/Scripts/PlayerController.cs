a1a922db24978ccce0d48ef4426bb8e4c0a37853
codeninjascf <cnchandlersford@gmail.com>
2023-03-25 11:45:54 +0000

    1 page

---
+++ PolyRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 25f;
+    void Start()
+    {
+      private bool _isGrounded;
+    private RigidBody2D rigidbody;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
