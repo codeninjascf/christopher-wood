d84d30278e3c5794fdf4f2de60b4ceea8cb0e4e8
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2023-12-04 17:36:53 +0000

    On script part 2 food frenzy

--- Food Frenzy!!!!/Assets/Scripts/GameManager.cs
+++ Food Frenzy!!!!/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,13 +10,44 @@ public class GameManager : MonoBehaviour
 
     private bool _gameEnded;
 
+    public UIManager uiManager;
+    
     void Start()
     {
         _gameEnded = false;
+
+        if(level.type == LevelType.Moves)
+        {
+            uiManager.criteriaInfoText.text = "MovesRemaining:";
+        }
+        else if
+        {
+            uiManager.criteriaInfoText.text = "Time Remaining:";
+        }
+
+        uiManager.highScoreNumber.text = level.HighScore.ToString();
+        uiManager.gameOverMenu.SetActive(false);
+
     }
 
     void Update()
     {
+        if(level.type == LevelType.Moves)
+        {
+            uiManager.criteriaNumber.text = level.MovesRemaining.ToString();
+        }
+        else if(level.type == LevelType.Time)
+        {
+            double timeRemaining = Math.Max(level.TimeRemaining, 0);
+            TimeSpan time = TimeSpan.FromSeconds(timeRemaining);
+            uiManager.criteriaNumber.text = time.ToString(@"mm\:ss");
+        }
+
+        int starsAchieved - level.StarsAchieved(gridManager.Score);
+
+        uiManager.UpdateScores(gridManager.Score);
+        uiManager.UpdateStars(starsAchieved);
+        
         if (level.GameOver && gridManager.MoveComplete && !_gameEnded)
         {
             _gameEnded = true;
