9d9b1459387cab86738abc97423b36171e1c4584
codeninjascf <cnchandlersford@gmail.com>
2024-05-11 11:04:10 +0100

    Part 6 halfway

--- Amazing Ninja Worlds!!!/Assets/Scripts/RubiesDisplay.cs
+++ Amazing Ninja Worlds!!!/Assets/Scripts/RubiesDisplay.cs
@@ -6,17 +6,38 @@ public class RubiesDisplay : MonoBehaviour
 {
     public int levelNumber;
     public GameObject[] rubies;
+<<<<<<< HEAD
+    // Start is called before the first frame update
+=======
+>>>>>>> 02574cdd2d6a40b73f040208c19cf690e936134b
     void Start()
     {
         UpdateRubies();
     }
 
+<<<<<<< HEAD
+    // Update is called once per frame
+=======
     
+>>>>>>> 02574cdd2d6a40b73f040208c19cf690e936134b
     void Update()
     {
         
     }
 
+<<<<<<< HEAD
+    public void UpdateRubies(){
+        gameObject.SetActive(PlayerPrefs.GetInt("Level" + levelNumber+ 
+        "_Complete")!= 0);
+
+        for (int i = 0; i<3; i++){
+            rubies[i].SetActive(PlayerPrefs.GetInt("Level" + levelNumber + 
+            "Gem" + (i + 1), 0) == 1);
+        }
+    }
+
+
+=======
 
     public void UpdateRubies()
     {
@@ -30,4 +51,5 @@ public class RubiesDisplay : MonoBehaviour
         }
     }
 
+>>>>>>> 02574cdd2d6a40b73f040208c19cf690e936134b
 }
