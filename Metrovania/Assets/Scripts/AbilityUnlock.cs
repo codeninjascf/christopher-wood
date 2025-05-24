cfc96c2b147ba6f3986d98987c60ee34d47c7788
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-05-24 12:21:16 +0100

    Finished Part 6 and Guide images 1

---
+++ Metrovania/Assets/Scripts/AbilityUnlock.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityUnlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool unlockDoubleJump, unlockDash, unlockBecomeBall, unlockDropBomb;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerAbilityTracker player = other.GetComponentInParent<PlayerAbilityTracker>();
+
+            if(unlockDoubleJump)
+            {
+                player.canDoubleJump = true;
+            }
+            if(unlockDash)
+            {
+                player.canDash = true;
+            }
+            if (unlockBecomeBall)
+            {
+                player.canBecomeBall = true;
+            }
+            if (unlockDropBomb)
+            {
+                player.canDropBomb = true;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
